5e345dfe60b01b80e038ea3c9f41e94aac71284c
Maxime Burlat <maximeburlat@gmail.com>
2020-01-31 19:29:00 -0600

    added two simple buttons

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -11,6 +11,18 @@
         </v-row></v-carousel-item
       >
     </v-carousel>
+    <v-row align="center">
+      <v-col class="text-center" cols="12" sm="6">
+        <div class="my-6">
+          <v-btn x-large color="secondary">Sign Up</v-btn>
+        </div>
+      </v-col>
+      <v-col class="text-center" cols="12" sm="6"
+        ><div class="my-6">
+          <v-btn x-large color="primary">Login</v-btn>
+        </div></v-col
+      >
+    </v-row>
   </div>
 </template>
 
