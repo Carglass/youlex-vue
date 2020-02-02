523b67091167b7f75ee997c195697a0ccbee3fdd
Maxime Burlat <maximeburlat@gmail.com>
2020-02-02 13:22:49 -0600

    created a very basic feed component with sample data (need to be moved to graphQL)

---
+++ src/components/Feed.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-list flat>
+    <template v-for="feedItem in feedData">
+      <v-list-item two-line :key="feedItem.id">
+        <v-list-item-content>
+          <v-list-item-title>{{ feedItem.title }}</v-list-item-title>
+          <v-list-item-subtitle>{{
+            feedItem.description
+          }}</v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+      <v-divider :key="feedItem.id"></v-divider>
+    </template>
+  </v-list>
+</template>
+
+<script>
+export default {
+  name: "feed",
+  props: {
+    feedData: Array
+  }
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,24 +6,38 @@
     <highlight-carousel></highlight-carousel>
     <v-row align="center">
       <v-col class="text-center" cols="12" sm="6">
-        <div class="my-6">
-          <v-btn x-large color="secondary">Sign Up</v-btn>
-        </div>
+        <v-btn block color="secondary">Sign Up</v-btn>
       </v-col>
-      <v-col class="text-center" cols="12" sm="6"
-        ><div class="my-6">
-          <v-btn x-large color="primary">Login</v-btn>
-        </div></v-col
-      >
+      <v-col class="text-center" cols="12" sm="6">
+        <v-btn block color="primary">Login</v-btn>
+      </v-col>
     </v-row>
+    <feed :feedData="feed"></feed>
   </div>
 </template>
 
 <script>
 import HighlightCarousel from "./../components/HighlightCarousel";
+import Feed from "./../components/Feed";
 
 export default {
   name: "home",
-  components: { HighlightCarousel }
+  components: { HighlightCarousel, Feed },
+  data() {
+    return {
+      feed: [
+        {
+          title: "Congress published a new bill",
+          description: "Hello hello",
+          id: 1
+        },
+        {
+          title: "Patrick commented on Ecology bill",
+          description: "Hello hello",
+          id: 2
+        }
+      ]
+    };
+  }
 };
 </script>
