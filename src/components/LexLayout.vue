fb2c6c920d1b698a6bb474de361cc6bfb0676655
Maxime Burlat <maximeburlat@gmail.com>
2020-03-31 21:39:31 -0500

    added the concept of the side bar

--- src/components/LexLayout.vue
+++ src/components/LexLayout.vue
@@ -1,18 +1,53 @@
 <template>
   <div>
-    <h1>{{ lexData.title }}</h1>
-    <em>from {{ lexData.owner.name }}</em>
-    <blockquote>{{ lexData.description }}</blockquote>
-    <blockquote>{{ lexData.contents.preambule }}</blockquote>
-    <div v-for="section in lexData.contents.sections" :key="section.title">
-      <h2>{{ section.title }}</h2>
-      <div v-for="article in section.articles" :key="article.title">
-        <h3>{{ article.title }}</h3>
-        <div v-for="alinea in article.alineas" :key="alinea.text">
-          <p>{{ alinea.text }}</p>
+    <v-row>
+      <v-col lg="8">
+        <h1>{{ lexData.title }}</h1>
+        <em>from {{ lexData.owner.name }}</em>
+        <blockquote>{{ lexData.description }}</blockquote>
+        <blockquote>{{ lexData.contents.preambule }}</blockquote>
+        <div v-for="section in lexData.contents.sections" :key="section.title">
+          <h2>{{ section.title }}</h2>
+          <div v-for="article in section.articles" :key="article.title">
+            <h3>{{ article.title }}</h3>
+            <div v-for="alinea in article.alineas" :key="alinea.text">
+              <p>{{ alinea.text }}</p>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      </v-col>
+      <v-col lg="4">
+        <v-sheet elevation="3" tile="tile">
+          <v-row no-gutters justify="center">
+            <v-btn-toggle v-model="toggle_exclusive" tile mandatory>
+              <v-btn icon>
+                <v-icon>mdi-bank-transfer</v-icon>
+              </v-btn>
+
+              <v-btn icon>
+                <v-icon>mdi-history</v-icon>
+              </v-btn>
+
+              <v-btn icon>
+                <v-icon>mdi-comment-text-multiple</v-icon>
+              </v-btn>
+
+              <v-btn icon>
+                <v-icon>mdi-help</v-icon>
+              </v-btn>
+            </v-btn-toggle>
+          </v-row>
+          <v-row>
+            <v-col cols="12">
+              <div>
+                This is where you have commands for the Lex. They are sorted by
+                category (amendments, history, discussion, wiki)
+              </div>
+            </v-col>
+          </v-row>
+        </v-sheet>
+      </v-col>
+    </v-row>
   </div>
 </template>
 
@@ -21,6 +56,11 @@ export default {
   name: "lexLayout",
   props: {
     lexData: Object
+  },
+  data() {
+    return {
+      toggle_exclusive: 2
+    };
   }
 };
 </script>
